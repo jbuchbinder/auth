8af344dea0a10fc196b1f02d1b0e6b811a39f041
Jeff Buchbinder <jeff@ourexchange.net>
2015-06-25 11:56:40 -0400

    Adjust with a few suggestions from pull request #20

--- token.go
+++ token.go
@@ -5,18 +5,18 @@ import (
 	"net/http"
 )
 
-// AuthToken is the authorization token extracted from the request.
-type AuthToken string
+// Token is the authorization token extracted from the request.
+type Token string
 
-// TokenFunc returns a Handler that authenticates via an X-Token header using the provided function.
+// TokenFunc returns a Handler that authenticates via an Authentication: Bearer header using the provided function.
 // The function should return true for a valid token.
 func TokenFunc(authfn func(string) bool) martini.Handler {
 	return func(res http.ResponseWriter, req *http.Request, c martini.Context) {
-		auth := req.Header.Get("X-Token")
-		if len(auth) < 2 || !authfn(auth) {
+		auth := req.Header.Get("Authorization")
+		if len(auth) < 7 || auth[:7] != "Bearer " || !authfn(auth[7:]) {
 			http.Error(res, "Not Authorized", http.StatusUnauthorized)
 			return
 		}
-		c.Map(AuthToken(auth))
+		c.Map(Token(auth[7:]))
 	}
 }
